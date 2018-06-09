0d2e2335d3ceabf85bd3ac4476c72ea05fc5621b
larabellatin <40076607+larabellatin@users.noreply.github.com>
2018-06-09 16:13:08 -0500

    Update Programa Principal.py

--- Programa Principal.py
+++ Programa Principal.py
@@ -8,9 +8,31 @@ print("Podemos revisar los siguientes temas:"'\n')
 for i in Temas:
   print(f"{Temas.index(i)+1}. " + i)
 
+def yourturn():
+  a = input('\t')
+  return a
+
 def variables():
   ### info de variables
-    print("variables")
+    print("Lo primero que debes saber sobre las variables es que se usan para almacenar información de cualquier tipo.")
+    print("Se usan así:")
+    print('\t'"a = 1+2")
+    print("La variable 'a' almacena el valor de 1+2, o sea 3.")
+    print("Ahora tú intenta definir una variable b con el número 4 como valor en la siguiente línea.")
+    b = yourturn()
+    if b.replace(' ','') == "b=4":
+      print("¡Buen trabajo!")
+    else:
+      print("Tu respuesta debería verse similar a esto: b = 4")
+    print('')
+    print("Las variables también pueden almacenar texto.")
+    print("Para esto, deben ser declaradas entre comillas de la siguiente manera: a = 'valor'.")
+    print("En la siguiente línea declara una variable 'nombre' cuyo valor sea tu primer nombre.")
+    global nombre
+    c = yourturn().replace(' ','')
+    nombre = nombre.replace('nombre=','').replace('"', '').capitalize()
+    print(f"Un gusto conocerte, {nombre}")
+
 
 def estructurasdecontrolselectivas():
   ### info de variables
